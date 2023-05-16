8d6859c78f6836575da6c2224d6dfcabca83dde4
phamanhtu1996 <patpro2801@icloud.com>
2023-05-16 20:50:00 +0700

    change dmoj to tmath and remove django 2.2

--- dmoj_celery.py
+++ dmoj_celery.py
@@ -6,10 +6,7 @@ except ImportError:
     import dmoj_install_pymysql  # noqa: F401, imported for side effect
 
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dmoj.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tmath.settings')
 
 # noinspection PyUnresolvedReferences
-import django_2_2_pymysql_patch  # noqa: E402, I100, F401, I202, imported for side effect
-
-# noinspection PyUnresolvedReferences
-from dmoj.celery import app  # noqa: E402, F401, imported for side effect
+from tmath.celery import app  # noqa: E402, F401, imported for side effect
